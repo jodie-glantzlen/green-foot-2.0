e0d1e4e87dd1b4bed5325b7dbfcdb10d76b665ae
jodie-glantzlen <jodie.glantzlen@gmail.com>
2022-08-21 15:57:35 +1200

    Configure Auth0Provider

--- client/index.js
+++ client/index.js
@@ -6,17 +6,25 @@ import thunk from 'redux-thunk'
 import reducers from './reducers'
 import App from './components/App'
 import { BrowserRouter } from 'react-router-dom'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
 
 document.addEventListener('DOMContentLoaded', () => {
   render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>,
+    <Auth0Provider
+      domain={'kotare-2022-jodie.au.auth0.com'}
+      clientId={process.env.CLIENT_ID}
+      redirectUri={window.location.origin}
+      audience="https://green-foot-2/api"
+    >
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </Auth0Provider>,
     document.getElementById('app')
   )
 })
