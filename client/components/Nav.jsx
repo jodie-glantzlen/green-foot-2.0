beb7dc3061fcdbda29ca57499369bf4d9983bcf8
jodie-glantzlen <jodie.glantzlen@gmail.com>
2022-08-22 16:56:12 +1200

    start again auth from scratch

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,15 +1,12 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import { useAuth0 } from "@auth0/auth0-react"
 
 const Nav = () => {
-
-  const { isAuthenticated } = useAuth0
   return (
     <>
       <Link to='/'>Welcome</Link>
-      {isAuthenticated && <Link to='/home'>Home</Link>}
-      {isAuthenticated && <Link to='/tips'>Tips & Tricks</Link>}
+      <Link to='/home'>Home</Link>
+      <Link to='/tips'>Tips & Tricks</Link>
     </>
   )
 }
--- client/components/Welcome.jsx
+++ client/components/Welcome.jsx
@@ -1,35 +1,13 @@
 import React from "react"
-import Auth from './Auth'
-import Signup from "./Signup"
-import { useAuth0 } from '@auth0/auth0-react'
 
 const Welcome = () => {
-
-  const handleClick = () => {
-    window.location.href = '/home'
-  }
-
-  const { isAuthenticated } = useAuth0()
-
-  const getButtons = () => {
-    if (isAuthenticated) {
-      return <button onClick={handleClick}>
-        <Auth />
-      </button>
-    }
-    return <div>
-      <Signup />
-      <Auth />
-    </div>
-  }
-
   return (
     <>
       <h1>Welcome to Green Foot</h1>
-      {getButtons()}
+      <button>Sign Up</button>
+      <button>Log in</button>
     </>
   )
 }
 
 export default Welcome
-
