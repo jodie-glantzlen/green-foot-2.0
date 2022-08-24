4988e1eba24fe4dfbe207178b7174e5174462cd6
jodie-glantzlen <jodie.glantzlen@gmail.com>
2022-08-24 17:30:30 +1200

    Add id column to users table

--- server/db/migrations/20220824050641_users.js
+++ server/db/migrations/20220824050641_users.js
@@ -1,6 +1,7 @@
 exports.up = (knex) => {
   return knex.schema.createTable('users', (table) => {
-    table.string('auth0_id').primary()
+    table.increments('id')
+    table.string('auth0_id')
     table.string('email')
   })
 }
